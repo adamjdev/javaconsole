645c2a2dcdb607fac1b181ac9f9237074213e143
Phillip <Phillip@192.168.1.50>
2015-07-18 11:05:23 +1000

    Add a "/" in front of available commands Begin gathering parameters for /connect

--- src/main/java/net/phillm/javaconsole/ConsoleApp.java
+++ src/main/java/net/phillm/javaconsole/ConsoleApp.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package net.phillm.javaconsole;
+
+import com.jcraft.jsch.JSch;
 import java.util.Scanner;
 
 /**
@@ -17,43 +19,55 @@ public class ConsoleApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        	    // Specifies Version
-	    final String Version = "1.0.0.001";
-	    // Specifies Website
-	    final String Website = "www.phillm.net";
-	    // Specifies Github Repo
-	    final String Github = "https://github.com/phillmac/java-console-app-test";
-	    //Initialize main loop
-            boolean runLoop;
-            runLoop = true;
-	    
-	    
-	    System.out.println("Info: Type /info to get the build version!");
-            System.out.println("Info: Type /help to get a list of availble commands!");
-	    while(runLoop){ //loop untill /stop command
-                Scanner scanner = new Scanner(System.in);
-                if(scanner.hasNext("/info")) {
-                	System.out.println("Build Version: " + Version);
-                    System.out.println("Website: " + Website);
-                    System.out.println("View on Github: " + Github);
-	    	
-                } else if(scanner.hasNext("/website")) {
-                    System.out.println("Website: " + Website);
-                    
-                } else if(scanner.hasNext("/help")) {
-                    System.out.println("Info: Gives Info Such as Build Version, Website, and Github Link");
-                    System.out.println("Website: Gives The Developers Website");
-                    System.out.println("Help: Gives All Commands");
-                     System.out.println("Stop: Close the console");
-                    
-                } else if(scanner.hasNext("/stop")) {
-                    System.out.println("Stopping");
-                    runLoop = false;
-                    
-                } else {
-                    System.out.println("Commands Available: [Info, Website, Help, Stop]");
-                }
-            } 
+        // Specifies Version
+        final String Version = "1.0.0.001";
+        // Specifies Website
+        final String Website = "www.phillm.net";
+        // Specifies Github Repo
+        final String Github = "https://github.com/phillmac/java-console-app-test";
+        //Initialize main loop
+        boolean runLoop;
+        runLoop = true;
+
+        JSch ssh_Con;
+
+        System.out.println("Info: Type /info to get the build version!");
+        System.out.println("Info: Type /help to get a list of availble commands!");
+        while (runLoop) { //loop untill /stop command
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNext("/info")) {
+                System.out.println("Build Version: " + Version);
+                System.out.println("Website: " + Website);
+                System.out.println("View on Github: " + Github);
+
+            } else if (scanner.hasNext("/website")) {
+                System.out.println("Website: " + Website);
+
+            } else if (scanner.hasNext("/help")) {
+                System.out.println("Info: Gives Info Such as Build Version, Website, and Github Link");
+                System.out.println("Website: Gives The Developers Website");
+                System.out.println("Help: Gives All Commands");
+                System.out.println("Stop: Close the console");
+
+            } else if (scanner.hasNext("/stop")) {
+                System.out.println("Stopping");
+                runLoop = false;
+
+            } else if (scanner.hasNext("/connect")) {
+                String host = "";
+                            
+                String[] ssh_Parameters = scanner.nextLine().split(" ");
+                if (ssh_Parameters.length > 1) {
+                    host = ssh_Parameters[1];
+                    }
+                            
+                     System.out.println(host);
+                    //System.out.println("Enter host to connect to");
+
+            } else {
+                System.out.println("Commands Available: [/Info, /Website, /Help, /Connect, /Stop]");
+            }
+        }
     }
-    
+
 }
