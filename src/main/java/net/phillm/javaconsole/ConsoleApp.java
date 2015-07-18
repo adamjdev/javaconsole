d5e6ea68e66e9345125113047af82e132b274c6c
Phillip <Phillip@192.168.1.50>
2015-07-18 15:27:09 +1000

    Fixed invalid package

--- src/main/java/net/phillm/javaconsole/ConsoleApp.java
+++ src/main/java/net/phillm/javaconsole/ConsoleApp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.java.net.phillm.javaconsole;
+package net.phillm.javaconsole;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
